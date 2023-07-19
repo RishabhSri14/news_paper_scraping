92dddf5a4e86a8346de7b6048fe72c5ad3c79cb8
Rishabh Srivastava <rishabh14601@gmail.com>
2023-07-20 04:01:21 +0530

    Completed whole code of adding the images

--- Advert_Gallery/download_links.py
+++ Advert_Gallery/download_links.py
@@ -12,120 +12,25 @@ def download_image(image_url, save_path):
         print("Image downloaded successfully!")
     except requests.exceptions.RequestException as e:
         print(f"Error downloading image- {e}")
-data = ["https://newspaperads.ads2publish.com/wp-content/uploads/2021/01/@home-by-nilkamal-furniture-ad-bangalore-times-23-01-2021.jpg",
-"https://newspaperads.ads2publish.com/wp-content/uploads/2019/07/at-home-furniture-the-big-sale-additional-10-off-ad-bangalore-times-19-07-2019.png"
-, 
-"https://newspaperads.ads2publish.com/wp-content/uploads/2019/04/at-home-furniture-minimum-30-off-last-4-days-ad-bangalore-times-26-04-2019.png"
-,
 
-"https://newspaperads.ads2publish.com/wp-content/uploads/2019/04/at-home-furniture-summer-vibes-fest-upto-40-off-ad-times-of-india-bangalore-22-03-2019.png"
-,
+for brand in os.listdir('./Data'):
+    print('\nDownloading Images for brand: ',brand,'\n')
+    brand_name = brand[:brand.rfind('.')]
+    if brand_name not in os.listdir('./images'):
+        os.mkdir(f'./images/{brand_name}') 
+        
+    parent_image_folder = f'./images/{brand_name}/'
+    with open('./Data/'+brand) as f:
+        data = json.load(f)
+    print('Total number of images:', str(len(data)))
 
-"https://newspaperads.ads2publish.com/wp-content/uploads/2019/03/at-home-furniture-the-big-sale-flat-60-off-ad-times-of-india-bangalore-22-02-2019.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2019/01/at-home-furniture-big-sale-flat-60-off-ad-bangalore-times-18-01-2019.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2019/01/at-home-furniture-the-big-sale-upto-50-off-ad-times-of-india-bangalore-12-01-2019.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/12/at-home-furniture-the-big-sale-upto-50-off-ad-bangalore-times-28-12-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/12/at-home-furniture-big-sale-upto-50-off-ad-times-of-india-bangalore-14-12-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/12/at-home-furniture-anniversary-celebration-upto-40-off-ad-times-of-india-bangalore-30-11-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/11/at-home-furniture-anniversary-celebration-upto-40-off-ad-times-of-india-bangalore-23-11-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/10/at-home-furniture-the-celebration-collection-upto-50-off-ad-times-of-india-chennai-26-10-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/08/at-home-furniture-the-big-sale-flat-60-off-ad-times-of-india-bangalore-04-08-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/07/at-home-furniture-the-big-sale-flat-60-off-ad-times-of-india-bangalore-20-07-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/07/at-home-furniture-the-big-sale-flat-60-off-ad-times-of-india-bangalore-13-07-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/06/at-home-furniture-shades-of-the-season-festival-upto-40-off-ad-bangalore-times-01-06-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/05/shades-of-the-season-festival-up-to-40-off-at-home-ad-deccan-chronicle-hyderabad-18-05-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/04/at-home-furniture-super-sale-upto-60-off-11-most-amazing-days-ad-times-of-india-ahmedabad-20-04-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/04/at-home-furniture-super-sale-upto-60-off-11-most-amazing-days-ad-bangalore-times-13-04-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/02/at-home-the-big-sale-upto-51-off-ad-times-of-india-bangalore-23-02-2018.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2018/01/the-big-sale-upto-51-off-at-home-ad-the-hindu-bangalore-19-01-2018.jpg"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/12/home-the-big-sale-upto-51-off-ad-the-hindu-chennai-22-12-2017.jpg"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/12/@home-the-big-sale-upto-51-off-no-room-for-reasons-ad-ahmedabad-times-15-12-2017.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/11/at-home-furniture-sit-back-relax-ad-bombay-times-24-11-2017.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/11/@home-lets-make-it-a-november-to-remember-anniversary-sale-ad-ahmedabad-times-03-11-2017.jpg"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/10/at-hoe-furniture-upto-50-off-ad-bombay-times-28-10-2017.jpg"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/10/@home-the-grand-festive-celebration-upto-50-off-ad-times-of-india-ahmedabad-14-10-17.jpg"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/10/@home-the-grand-festive-celebration-upto-50-off-shop-at-the-nearest-store-or-visit-www-at-home-co-in-ad-deccan-chronicle-hyderabad-14-10-2017.png"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/10/at-home-furniture-the-grand-festive-celebration-upto-50-off-ad-bombay-times-29-09-2017.jpg"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/09/@home-furniture-the-grand-festive-celebration-upto-50-off-ad-ahmedabad-times-22-9-17.jpg"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/09/@home-shop-this-look-at-out-store-or-visit-online-last-3-days-upto-50-off-ad-chennai-times-02-9-17.jpg"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/08/custom-kitchens-@home-your-kitchen-your-way-ad-property-times-mumbai-25-8-17.jpg"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/08/@home-the-freedom-sale-special-offers-upto-50-ad-times-of-india-bangalore-12-08-2017.jpg"
-,
-
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/08/@home-the-big-sale-ad-chennai-times-14-7-2017.jpg"
-,
-"https://newspaperads.ads2publish.com/wp-content/uploads/2017/08/@home-furniture-the-big-sale-last-4-days-ad-bangalore-times-28-7-2017.jpg"
-]
-
-brand = 'home'
-if brand not in os.listdir('./images'):
-    os.mkdir(f'./images/{brand}') 
-    
-parent_image_folder = f'./images/{brand}/'
-print('Total number of images:', str(len(data)))
-
-for i, links in enumerate(data):
-    ext = links[links.rfind('.'):]
-    # time.sleep(5)
-    print(f'Downloading image number {i+1}...')
-    urllib.request.urlretrieve(links, parent_image_folder+str(i+400)+ext)
-    #OR
+    for i, obj in enumerate(data):
+        links = obj['url']
+        ext = links[links.rfind('.'):]
+        # time.sleep(5)
+        print(f'Downloading image number {i+1}...')
+        urllib.request.urlretrieve(links, parent_image_folder+str(i+400)+ext)
+        #OR
     # download_image(links, f'{i+1}{ext}')
         
     
--- Advert_Gallery/scrapper/Extract_list.js
+++ Advert_Gallery/scrapper/Extract_list.js
@@ -1,113 +1,81 @@
-
-var fs = require("fs");
-function extract_brand_links(){
-    // First go to https://www.advertgallery.com/product-category/advertisements-by-brand
-    console.log("Extracting Brand Links")
-    x = document.getElementsByClassName("products columns-4")[0].children
-    for (var i = 0; i < x.length; i++){
-        html = x[i].innerHTML
-        link = html.split('"')[3]
-        console.log(link)
-    }
+const puppeteer = require('puppeteer');
+var fs = require('fs')
+const brand_names = require('./brand_names.json')
+function extract_brand_links() {
+  // First go to https://www.advertgallery.com/product-category/advertisements-by-brand
+  console.log("Extracting Brand Links")
+  x = document.getElementsByClassName("products columns-4")[0].children
+  for (var i = 0; i < x.length; i++) {
+    html = x[i].innerHTML
+    link = html.split('"')[3]
+    console.log(link)
+  }
 
 }
-function extract_img_links(brand){
-    // First go to https://www.advertgallery.com/product-category/advertisements-by-brand/<brand_name>
-    x = document.getElementsByClassName("products columns-4")[0].children
-    var data =[]
-    for (var i = 0; i < x.length; i++){
-        html = x[i].innerHTML
-        web_link = html.substring(html.indexOf("href=")+6).split('"')[0]
-        thumb_link = html.substring(html.indexOf("src=")+5).split('"')[0]
-        img_link = thumb_link.substring(0,thumb_link.lastIndexOf('-'))+thumb_link.substring(thumb_link.lastIndexOf('.'))
-        // console.log(img_link)
-        data.push({'Sno': 400+i,'title': title,'url': img_link, 'taken from': web_link})
-    }
-    fs.writeFile("../Data/"+brand+".json", JSON.stringify(data), err => {
-     
-      if (err) throw err; 
-     
-      console.log("Done writing"); // Success
+function extract_img_links(brand) {
+  // First go to https://www.advertgallery.com/product-category/advertisements-by-brand/<brand_name>
+  x = document.getElementsByClassName("products columns-4")[0].children
+  var data = []
+  for (var i = 0; i < x.length; i++) {
+    html = x[i].innerHTML
+    web_link = html.substring(html.indexOf("href=") + 6).split('"')[0]
+    thumb_link = html.substring(html.indexOf("src=") + 5).split('"')[0]
+    img_link = thumb_link.substring(0, thumb_link.lastIndexOf('-')) + thumb_link.substring(thumb_link.lastIndexOf('.'))
+    // console.log(img_link)
+    data.push({ 'Sno': 400 + i, 'title': title, 'url': img_link, 'taken from': web_link })
+  }
+  fs.writeFile("../Data/" + brand + ".json", JSON.stringify(data), err => {
+
+    if (err) throw err;
+
+    console.log("Done writing"); // Success
   });
 }
-var brand ='home'
-var url = 'https://www.advertgallery.com/product-category/advertisements-by-brand/'+brand+'/'
-// window.open('https://www.advertgallery.com/product-category/advertisements-by-brand/'+brand+'/');
-// setTimeout(function() {
-//     // Call your JavaScript function here
-//     extract_brand_links();
-//   }, 10000);
-// // window.close();
-
-
-// const open = require('open');
-
-// // Open the URL
-// open(url)
-//   .then(() => {
-//     // Execute your JavaScript function here
-//     extract_img_links();
-//   })
-//   .catch(err => {
-//     console.error('Error:', err);
-//   });
-
-
-const puppeteer = require('puppeteer');
 
 async function openURLAndExecuteFunction(url) {
   try {
     // const browser = await puppeteer.launch({executablePath: '/mnt/c/Users/risha/Downloads/chromedriver.exe', timeout: 60000, headless: false});
     // console.log('YESSSSSSSS')
-    const browser = await puppeteer.launch({
-      executablePath: '/mnt/c/Program Files/Google/Chrome/Application/chrome.exe', // Replace with the actual path to your Chrome executable on Windows
-      // args: ['--no-sandbox'], // Optionally disable sandbox if needed
-      headless: false,
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    // Execute your JavaScript function here
-    await page.evaluate(() => {
-      extract_img_links(brand);
+    const data = await page.evaluate(() => {
+      var data = []
+      x = document.getElementsByClassName("products columns-4")[0].children
+      for (var i = 0; i < x.length; i++) {
+        html = x[i].innerHTML
+        web_link = html.substring(html.indexOf("href=") + 6).split('"')[0]
+        thumb_link = html.substring(html.indexOf("src=") + 5).split('"')[0]
+        img_link = thumb_link.substring(0, thumb_link.lastIndexOf('-')) + thumb_link.substring(thumb_link.lastIndexOf('.'))
+        temp = img_link.split('/')
+        var title = (temp[temp.length - 1].substring(0,temp[temp.length - 1].lastIndexOf('.'))).replace('-',' ')
+        // console.log(img_link)
+        data.push({ 'Sno': 400 + i, 'title': title, 'url': img_link, 'taken from': web_link })
+      }      
+      return data;
     });
-
     await browser.close();
+    return data;
   } catch (err) {
     console.error('Error:', err);
   }
 }
 
-// Replace 'https://www.example.com' with the URL you want to open
-openURLAndExecuteFunction(url);
+const result = async function(){
+  for (var i =0;i<brand_names.length;i++){
 
-// const axios = require('axios');
-// const { JSDOM } = require('jsdom');
-// const { window } = new JSDOM('<!DOCTYPE html><html><body></body></html>');
-// const $ = require('jquery')(window);
-// async function openURLAndExecuteFunction(url, jsFunction) {
-//   try {
-//     const response = await axios.get(url);
-//     const dom = new JSDOM(response.data, { runScripts: 'dangerously', resources: 'usable', window });
-//     const result = dom.window.eval(jsFunction);
-
-//     return result;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
-// // Example usage:
-// const jsFunction = () => {
-//   // Replace this function with the JavaScript function you want to execute on the page
-//   // Your code can now use the injected jQuery instance as '$' or 'jQuery'
-//   return $('title').text(); // Return the page title using jQuery
-// };
-
-// openURLAndExecuteFunction(url, jsFunction)
-//   .then((result) => {
-//     console.log('Result:', result);
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error);
-//   });
-
+    var brand = brand_names[i]
+    var url = 'https://www.advertgallery.com/product-category/advertisements-by-brand/' + brand + '/'
+  
+    const json_data = await openURLAndExecuteFunction(url);
+    console.log(json_data)
+    fs.writeFile("../Data/" + brand + ".json", JSON.stringify(json_data), err => {
+    
+      if (err) throw err;
+    
+      console.log("Done writing"); // Success
+    });
+  }
+  return json_data 
+}
+result()
